aa458f1ac55cf98d9f22c359a04d74cf5f9c6ff7
kenneth <johnkennethgibasabella@gmail.com>
2017-02-23 14:52:11 +0800

    Made the category field in materials form non-mandatory

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -76,9 +76,7 @@ class Form extends CI_Controller {
 		$this->load->view('pages/navigation.php',self::get_parent_categories());
 
 		$this->form_validation->set_error_delimiters('<br/><pclass="text-danger">', '</p>');
-
 		$this->form_validation->set_rules('title', 'Title', 'required');
-		$this->form_validation->set_rules('creator', 'Creator', 'required');
 		$this->form_validation->set_rules('content_description', 'Description', 'required');
 
 		if ($this->form_validation->run() == FALSE)
