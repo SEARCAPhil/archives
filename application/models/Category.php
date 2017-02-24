7c4cbf90da4e9254351bba5ab22bbe3f48b552e1
kenneth <johnkennethgibasabella@gmail.com>
2017-02-24 10:40:48 +0800

    Show Categories and Sub-Categories in Alphabetical order

--- application/models/Category.php
+++ application/models/Category.php
@@ -8,12 +8,12 @@ class Category extends CI_Model {
 	}
 
 	public function get_parent_categories(){
-		$stmt = $this->db->query("SELECT * FROM category where parent_id=0");
+		$stmt = $this->db->query("SELECT * FROM category where parent_id=0 ORDER by category ASC");
 		return $stmt->result();
 	}
 
 	public function get_parent_categories_for_user_only(){
-		$stmt = $this->db->query("SELECT * FROM category where parent_id=0 and is_private=0");
+		$stmt = $this->db->query("SELECT * FROM category where parent_id=0 and is_private=0 ORDER by category ASC");
 		return $stmt->result();
 	}
 
@@ -21,14 +21,14 @@ class Category extends CI_Model {
 
 	public function get_children_categories($parent_id){
 		$this->parent_id=(int) htmlentities(htmlspecialchars($parent_id));
-		$query="SELECT * FROM category where parent_id=?";
+		$query="SELECT * FROM category where parent_id=? ORDER by category ASC";
 		$stmt=$this->db->query($query,array($this->parent_id));
 		return $stmt->result();
 	}
 
 	public function get_children_categories_for_user_only($parent_id){
 		$this->parent_id=(int) htmlentities(htmlspecialchars($parent_id));
-		$query="SELECT * FROM category where parent_id=? and is_private=0";
+		$query="SELECT * FROM category where parent_id=? and is_private=0 ORDER by category ASC";
 		$stmt=$this->db->query($query,array($this->parent_id));
 		return $stmt->result();
 	}
