79cc7f6a5a304ea07716c7361bbfa3538d993402
kenneth <johnkennethgibasabella@gmail.com>
2017-03-14 17:03:15 +0800

    Change maintenance page color

--- application/views/pages/maintenance.php
+++ application/views/pages/maintenance.php
@@ -10,7 +10,7 @@
 		::-moz-selection { background-color: #E13300; color: white; }
 
 		body {
-			background-color: rgb(53,175,186);
+			background-color: rgb(255,169,18);
 			margin: 40px;
 			font: 13px/20px normal Helvetica, Arial, sans-serif;
 			color: rgb(255,255,255);
@@ -26,10 +26,10 @@
 			color: rgb(255,255,255);
 			background-color: transparent;
 			border-bottom: 1px solid #D0D0D0;
-			font-size: 19px;
+			font-size: 5em;
 			font-weight: normal;
 			margin: 0 0 14px 0;
-			padding: 14px 15px 10px 15px;
+			padding: 14px 15px 20px 15px;
 		}
 
 		code {
@@ -51,14 +51,19 @@
 
 		p {
 			margin: 12px 15px 12px 15px;
+			font-size: 2em;
+			line-height: 30px;
 		}
 		</style>
 	</head>
 <body>
-	<div style="width:20%;text-align: center;margin-left: 40%;">
-		<img src="<?php echo base_url(); ?>assets/images/maintenance.png" width="300px">
-		<h1><b>Maintenance</b></h1>
-		<p>Sorry, we are currently improving the system. Please check back within <b>24 hours</b>.</p>
+	<div style="width:30%;text-align: center;margin-left: 35%;">
+		<center>
+			<img src="<?php echo base_url(); ?>assets/images/maintenance.png" width="300px">
+			<h1><b>Maintenance</b></h1>
+			<p>Sorry, we are currently improving the system. Please check back within <b>24 hours</b>.</p>
+		</center>
 	</div>
+
 </body>
 </html>
