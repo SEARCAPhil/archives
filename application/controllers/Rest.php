543c6b73b012f6803fba29e254699813fb4d951d
kenneth <johnkennethgibasabella@gmail.com>
2017-03-22 17:36:41 +0800

    Added restriction in download page

--- application/controllers/Rest.php
+++ application/controllers/Rest.php
@@ -27,20 +27,41 @@ class Rest extends MY_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->database();
-		$this->load->model('category');
-		$this->load->model('item');
+		$this->load->model(array('privilege','category','item'));
 		$this->load->helper('url');
+		$this->load->library(array('form_validation','session'));
 
 		parent::set_maintenance();
+		self::load_privileges();
 	}
 
-	
+		/*
+	 * Load account privileges
+	 *
+	 * */
+
+	public function load_privileges(){
+		$this->session_privileges=($this->privilege->get_privilege(@$_SESSION['priv']));
+	}
+
+	public function load_menu(){
+		
+		$this->menu=array(
+			'category'=>1,
+			'permission'=>$this->privilege->is_allowed_to_grant_role(),
+			'materials'=>$this->privilege->is_allowed_to_write_materials()
+
+		);
+
+
+	}
+
 
 	public function series(){
 		$data=json_decode($this->input->raw_input_stream);
 		
 
-		echo json_encode($this->category->get_children_categories($data->id));
+		echo json_encode($this->category->get_children_categories(@$this->session_privileges[0]->role_id,$data->id));
 	}
 
 	public function remove(){
@@ -51,17 +72,16 @@ class Rest extends MY_Controller {
 
 	public function file_download(){
 
-		$details=$this->item->get_item_category($this->input->get('id',true));
+		$details=$this->item->get_item_details($this->input->get('id',true));
 		$dir='./uploads/';
 		
-		
 	
-		$base=$this->category->get_category_details(@$details[0]->cat_id);
+		$base=$this->category->get_category_details(@$this->session_privileges[0]->role_id,@$details[0]->cat_id);
 		$dir.=@$base[0]->id.'/'.$this->input->get('id',true).'/'.$details[0]->file_name;
 
-		
-		
-		if(file_exists($dir)&&isset($details[0]->cat_id)&&is_file($dir)){
+		#must be able to have read privilege
+		if(@$base[0]->read_privilege==1){
+			if(file_exists($dir)&&isset($details[0]->cat_id)&&is_file($dir)){
 
 			$returnFile=header("Content-Description: File Transfer"); 
 			$returnFile+=header("Content-Type: application/octet-stream"); 
@@ -72,11 +92,13 @@ class Rest extends MY_Controller {
 			
 			echo $returnFile;
 
+			}else{
+				echo '<center><h1><br/><br/>Oops!! File not found!</h1><p>The file you are trying to access is currently unavailable. Please try again later.</p></center>';
+			}
+
 		}else{
 			echo '<center><h1><br/><br/>Oops!! File not found!</h1><p>The file you are trying to access is currently unavailable. Please try again later.</p></center>';
 		}
-		
-		
 
 	}
 
