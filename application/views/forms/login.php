696b03b61d07c518c5b63056c5a830bbace32c1d
kenneth <johnkennethgibasabella@gmail.com>
2018-06-06 11:04:40 +0800

    Fixes #16 - Changed login page layout

--- application/views/forms/login.php
+++ application/views/forms/login.php
@@ -1,105 +1,134 @@
+<style>
+    .main-login {
+        height:100vh;
+        overflow:hidden;
+    }
+    .main-login-banner {
+        height:100vh;
+        padding-top:20%;
+        color:rgb(255,255,255);
+        z-index:1;
+        position:relative;
+    }
 
-<div style="margin-top: 4%;">
+    .main-login-backdrop {
+        float:left;
+        position:absolute;
+        width:100%;
+        height:100%;
+        top:0;
+         background:rgba(0,0,0,0.8);
+         z-index:1;
+    }
 
-<!--login form-->
-<div class="loginForm ">
+    .main-login-banner-text {
+        float:left;
+        position:absolute;
+        width:100%;
+        height:100%;
+        top:40vh;
+        z-index:2;
+    }
 
 
+    .green {
+        color: #23f98b;
+    }
 
-	<!--<div class="page-header col col-md-10 row">
-		<h2>Supporting Documents for Financial Transaction <small>Management System</small></h2>
-	</div>-->
-
- 
-<?php echo form_open('home/authentication','class="form-horizontal",action='.$_SERVER['PHP_SELF']); ?>
-
-<style type="text/css">
-  input[type="text"],input[type="password"]{
-    border:none;
-    border-bottom: 1px solid #ccc !important;
-    border-radius: 0 !important;
-    -webkit-border-radius: 0 !important;
-    padding:20px;
-    box-shadow: none;
-  }
-
-  input[type="text"]:focus,input[type="password"]:focus{
-    box-shadow: none;
-  }
-
-  .btn-submit{
-    background: rgb(55,164,249);
-    color:rgb(255,255,255);
-    padding: 10px;
-    border-radius: 20px;
-  }
-
-  .searca-about,.searca-footer{
-    font-size: 9px;
-    margin-top: 50px;
-  }
-  .searca-footer{
-    line-height: 0;
-    margin-top: 60px;
-  }
-  .alert-danger{
-    background: rgba(255,99,33,0.3);
-  }
+    .sub-login-banner {
+        padding-top:7%;
+        position:relative;
+        height:100%;
+        overflow:hidden;
+        overflow-y: auto;
+    }
+    .go-to-app-btn {
+        border-radius: 50px;
+    }
+    .doodle-section {
+        height:100%;
+        width:100%;
+        background:url('assets/images/doodle.png') no-repeat;
+        overflow:hidden;
+        position:absolute;
+        z-index:0;
+        top:35%;
+    }
+    .sub-banner-deco {
+        position:absolute;
+        height:100px;
+        width:100%;
+        background:url('assets/images/sub-banner-deco.png') no-repeat;
+        background-size:cover;
+        overflow:hidden;
+        bottom:0;
+    }
 </style>
-  
-<div class="col col-lg-4 col-lg-offset-4 col-md-5 col-md-offset-4 col-sm-7 col-sm-offset-3 col-xs-10 col-xs-offset-1">	
+<article class="main-login row">
+    <section class="col col-lg-7 main-login-banner">
+        <div class="container col-lg-offset-1 col-sm-offset-2 col-xs-offset-1 main-login-banner-text">
+            <h2>Documents Management System</h2>
+            <p class="text-muted">Manage and <span class="green">access</span> your files <span class="green">everywhere</span></small></p>
 
-        <div class="row">
-         <div class="col col-md-12 text-center" style="min-width: 70px !important;margin-bottom: 40px;">
-            <!--  <img src="<?php echo base_url(); ?>assets/images/logo-new.png" width="100px">-->
-            <img src="<?php echo base_url(); ?>assets/images/SEARCA.png" width="70%">
-            
-          </div>
-
-          <div class="col col-md-12 text-center" style="margin-bottom: 20px;">
-            <h1 style="padding-right: 0px !important;"> Sign In </h1>
-            <p><small class="text-muted">Document Management System</b></small></p>
-          </div>
+            <button class="btn btn-office365 btn-lg btn-dark btn-submit go-to-app-btn" type="button" style="color:#fff;background:#ff0763">Login with Office365</button>
         </div>
-
-		  <div class="form-group col col-md-12 text-center"  style="margin-bottom: 30px;">
-     	  		<input type="text" class="form-control text-center" id="inputUser" placeholder="Username" name="username" required autocomplete="off">   	   
-   		 		<!--<span class="glyphicon glyphicon-user form-control-feedback"></span>-->
-   		 </div>
-   		 <div class="form-group col col-md-12"  style="line-height: 0;margin-bottom: 30px;">
-     	  
-     	   <div>
-        		<input type="password" class="form-control text-center" placeholder="Password" id="inputPassword" name="password" required>
-     	   </div>
-   		 </div>
+        <div class="main-login-backdrop"></div>
+        <div class="doodle-section"></div>
+    </section>
 
 
 
-        <?php if($data){ ?>
-          <div class=" col col-md-12 row">
-            <div class="alert alert-danger">
-              <p>Oopss! Invalid username or password!</p>
-              <p class="text-important">Is this your account? <small><a href="<?php echo base_url(); ?>">not my account.</a></small></p>
+    <section class="col col-lg-5 sub-login-banner">
+        <div class="sub-banner-deco"></div>
+        <div class="col-lg-10 col-lg-offset-1">
+            <img src="<?php echo base_url(); ?>assets/images/searca-new.png" width="150px">
+            <p>What makes it a powerful  system for handling <br/>your records and documents?</p>
+            <br/>
+
+
+            <div class="media">
+                <div class="media-left">
+                <img src="<?php echo base_url(); ?>assets/images/checked-green.png" width="50px">
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Well keep and organized</h4>
+                    <p><small>Excellent feature! I love it. One day I'm definitely going to put this Bootstrap component into use and I'll let you know once I do.</small></p>
+                </div>
+                <hr/>
             </div>
-          </div>
-        <?php } ?>
 
+             <div class="media">
+                <div class="media-left">
+                <img src="<?php echo base_url(); ?>assets/images/checked-green.png" width="50px">
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Safe and secured</h4>
+                    <p><small>Excellent feature! I love it. One day I'm definitely going to put this Bootstrap component into use and I'll let you know once I do.</small></p>
+                </div>
+                <hr/>
+            </div>
 
+             <div class="media">
+                <div class="media-left">
+                <img src="<?php echo base_url(); ?>assets/images/checked-green.png" width="50px">
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">Accessible everywhere</h4>
+                    <p><small>Excellent feature! I love it. One day I'm definitely going to put this Bootstrap component into use and I'll let you know once I do.</small></p>
+                </div>
+                <hr/>
+            </div>
 
-       <!--submit-button-->
-   		 <div class="form-group col col-md-12 " style="margin-top: 30px;">
-    		<button class="btn btn-block btn-submit">Login</button>
-  		 </div>
+            <br/>
+            <br/>
+            <p>
+                <small>By signing in to DTS you agree to <span class="text-success">Users License agreement</span> and <span class="text-success">Data policy</span> settings of SEARCA</small>
+            </p>
 
-
-</div>
-
-<!--end login-form-->
-
-
-</div>
-
-
+        </div> 
+        
+        
+    </section>
 
 <!--end content-->
 </artice>
